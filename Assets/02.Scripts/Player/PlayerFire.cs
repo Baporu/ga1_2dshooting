bad34f5dfe9d329c20764b48dddff60bad2448d0
mongilteacher <mongilteacher@gmail.com>
2026-09-02 16:08:48 +0900

    feat: 총알 발사에 있어 쿨타임 적용

--- Assets/02.Scripts/Player/PlayerFire.cs
+++ Assets/02.Scripts/Player/PlayerFire.cs
@@ -12,26 +12,40 @@ public class PlayerFire : MonoBehaviour
     public Transform LeftFirePoint;
     public Transform RightFirePoint;
     
-    // public Transform[] FirePoints;
+    // - 쿨타이머
+    public float CoolTime = 0.5f;
+    public float CoolTimer = 0;
+
+    private void Start()
+    {
+        CoolTimer = CoolTime;
+    }
     
     
     private void Update()
     {
-        Fire();
+        // 0. 쿨타이머 감소
+        CoolTimer -= Time.deltaTime;
+        
+        // 1. 쿨타이머가 0초 이하이고 && 스페이스바를 누르면 
+        if (CoolTimer <= 0 && Input.GetKeyDown(KeyCode.Space))
+        {
+            // 2. 발사
+            Fire();
+            
+            // 3. 쿨타이머 초기화
+            CoolTimer = CoolTime;
+        }
     }
 
     private void Fire()
     {
-        // 1. 스페이스바를 누르면 
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            // 2. 총알 프리팹을 생성한다.
-            // Instantiate는 프리팹을 복사해서 (Monobehaviour를 상속받는)게임 오브젝트를 생성하고 씬에 넣어주는 기능
-            GameObject leftBullet = Instantiate(BulletPrefab);
-            leftBullet.transform.position = LeftFirePoint.position;  // 생성한 총알의 위치를 총구의 위치로
-            
-            GameObject rightBullet = Instantiate(BulletPrefab);
-            rightBullet.transform.position = RightFirePoint.position;  // 생성한 총알의 위치를 총구의 위치로
-        }
+        // 2. 총알 프리팹을 생성한다.
+        // Instantiate는 프리팹을 복사해서 (Monobehaviour를 상속받는)게임 오브젝트를 생성하고 씬에 넣어주는 기능
+        GameObject leftBullet = Instantiate(BulletPrefab);
+        leftBullet.transform.position = LeftFirePoint.position;  // 생성한 총알의 위치를 총구의 위치로
+        
+        GameObject rightBullet = Instantiate(BulletPrefab);
+        rightBullet.transform.position = RightFirePoint.position;  // 생성한 총알의 위치를 총구의 위치로
     }
 }

295d240d1428075510c86e66f07f72a537b069d3
mongilteacher <mongilteacher@gmail.com>
2026-09-02 13:46:37 +0900

    feat: 캐릭터 이동 제한과 스피드 업/다운

--- Assets/02.Scripts/Player/PlayerMove.cs
+++ Assets/02.Scripts/Player/PlayerMove.cs
@@ -6,6 +6,10 @@ public class PlayerMove : MonoBehaviour
  
     // 필요 필드:
     public float Speed;
+    public float MaxPositionY;
+    public float MinPositionY;
+    public float MaxPositionX;
+    public float MinPositionX;
     
     
     // 매 프레임마다 실행된다.
@@ -13,25 +17,47 @@ public class PlayerMove : MonoBehaviour
     private void Update()
     {
         // 1. 키보드 입력을 받는다.
-        float h = Input.GetAxisRaw("Horizontal"); // 키보드 왼/오른쪽 입력 상태에 따라 -1f ~ 0 ~ 1f
-        float v = Input.GetAxisRaw("Vertical");   // 키보드 위/아래 입력 상태에 따라 -1f ~ 0 ~ 1f
-        
-        Debug.Log($"h:{h}, v:{v}");
+        float h = Input.GetAxisRaw("Horizontal"); 
+        float v = Input.GetAxisRaw("Vertical");
         
         // 2. 키보드 입력에 따라 방향을 구한다.
-        // 게임에는 벡터라는 타입이 있다. 벡터는(크기와 방향을 의미한다)
-        Vector2 direction = new Vector2(h, v); // 왼쪽 방향
-        // = Vector2 direction = Vector2.left;
+        Vector2 normalizedDirection = new Vector2(h, v).normalized;
 
         // 3. 방향과 속력에 따라 이동한다.
-        // 속도 = 방향 * 속력                        // 매직 넘버란: 보는 사람에 따라 의미가 달라질 수 있는
-        // 헷갈리는 숫자
-        // 0.06
-        Vector2 normalizedSpeed = (direction * Speed).normalized; // 벡터의 길이를 1로 만들어주는것 (즉, 방향만 유지한다.)
-        transform.Translate(normalizedSpeed * Time.deltaTime);
-        // deltaTime: 이전 프레임으로부터 지금 프레임까지 시간이 얼마나 지났는지 MS로 반환
+        Vector2 newPosition = transform.position + (Vector3)normalizedDirection * Speed * Time.deltaTime;
+
+        // 4. 위치 y에 제한이 있다.
+        if (newPosition.y > MaxPositionY)
+        {
+            newPosition.y = MaxPositionY;
+        }
+        else if (newPosition.y < MinPositionY)
+        {
+            newPosition.y = MinPositionY;
+        }
         
-        // 새로운 위치 = 현재 위치 + (방향 * 속력 * 시간)
-        // transform.position += (Vector3)direction * Speed * Time.deltaTime;
+        // 5. 양 옆 끝으로 가면 반대쪽 방향으로 이동
+        if (newPosition.x > MaxPositionX)
+        {
+            newPosition.x = MinPositionX;
+        }
+        else if (newPosition.x < MinPositionX)
+        {
+            newPosition.x = MaxPositionX;
+        }
+        
+        transform.position = newPosition;
+
+
+        // 7. Q/E 버튼 입력을 통한 스피드 업/다운
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Speed++;
+        }
+        else if (Input.GetKeyDown(KeyCode.Q))
+        {
+            Speed--;
+        }
     }
+    
 }

69b402e391b22eccb91674f6a6ce3148ec29ca41
LAPTOP-F2GOMJH5\qkswk <vitaqin@naver.com>
2026-09-03 12:57:43 +0900

    feat: 총알 충돌 처리 추가

--- Assets/02.Scripts/Bullet/Bullet.cs
+++ Assets/02.Scripts/Bullet/Bullet.cs
@@ -1,14 +1,34 @@
+using System;
 using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
     // 목적: 총알을 위로 움직이고 싶다.
-
-    public float Speed;
+    public float MoveSpeed;
 
     private void Update()
     {
         Vector2 direction = Vector2.up; //  new Vector2(0, 1);
-        transform.Translate(direction * Speed * Time.deltaTime);
+        transform.Translate(direction * MoveSpeed * Time.deltaTime);
+    }
+
+    // 충돌 관련 이벤트 (Enter -> Stay -> Exit)
+    // 충돌이 시작되면 호출되는 이벤트 함수
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("충돌했다!");
+
+        Destroy(this.gameObject);
+        Destroy(collision.gameObject);
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        //Debug.Log("충돌 중이다!");
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        //Debug.Log("충돌 끝났다!");
     }
 }

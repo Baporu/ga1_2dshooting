05aba1f6b3f5bce2b57ded5a5be548a9611bdf28
mongilteacher <mongilteacher@gmail.com>
2026-09-08 11:18:14 +0900

    feat: Aimed와 Homimg 타입이 플레이어 바라보도록 회전하기
    - Aimed: 시작할 때 한 번
    - Homig: 매 업데이트마다

--- Assets/02.Scripts/Enemy/AimedEnemy.cs
+++ Assets/02.Scripts/Enemy/AimedEnemy.cs
@@ -15,6 +15,17 @@ public class AimedEnemy : Enemy
         }
 
         _direction = _player.transform.position - transform.position;
+
+        float dx = _direction.x; // 플레이어와 에너미 사이의 밑변 길이
+        float dy = _direction.y; // 플레이어와 에너미 사이의 높이 길이
+        // tan세타 = dy / dx
+        // tan^ * tan세타 = tan^ * dy / dx
+        // 세타 = tant^ * dy / dx
+        // 각도 = 세타 * Rad2Deg
+        float radian = Mathf.Atan2(dy, dx);
+        float angle = radian * Mathf.Rad2Deg + 90;
+        transform.rotation = Quaternion.Euler(0f, 0f, angle);
+
         _direction.Normalize();
     }
 
@@ -23,6 +34,7 @@ public class AimedEnemy : Enemy
         if (_player == null) return;
 
         //  방향과 속도에 맞게 이동한다.
-        transform.Translate(_direction * _moveSpeed * Time.deltaTime);
+        //transform.Translate(_direction * _moveSpeed * Time.deltaTime);
+        transform.position += (Vector3)(_direction * _moveSpeed) * Time.deltaTime;
     }
 }

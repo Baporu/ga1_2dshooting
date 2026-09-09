49e5f831dd98b6579ed9d4a98b7f621fbbe3cf5b
LAPTOP-F2GOMJH5\qkswk <vitaqin@naver.com>
2026-09-09 12:13:26 +0900

    refactor: 폭탄 애니메이션을 코드에서 제어

--- Assets/02.Scripts/Item/Bomb.cs
+++ Assets/02.Scripts/Item/Bomb.cs
@@ -1,25 +1,22 @@
-using System;
+using System.Collections;
 using UnityEngine;
 
 public class Bomb : MonoBehaviour
 {
+    [Header("Bomb")]
     [SerializeField] private int _damage = 99999;
     [SerializeField] private float _duration = 3f;
-    private float _timer;
+
+    [Header("Animation")]
+    [SerializeField] private Vector2 _minMaxScale = new Vector2(4.8f, 5.2f);
+
+    [SerializeField] private float _expandTime = 0.2f;
+    [SerializeField] private float _shrinkTime = 0.2f;
+
 
     private void Start()
     {
-        _timer = _duration;
-    }
-
-    private void Update()
-    {
-        _timer -= Time.deltaTime;
-
-        if (_timer > 0)
-            return;
-
-        Destroy(gameObject);
+        StartCoroutine(ExpandBomb());
     }
 
     private void OnTriggerEnter2D(Collider2D collider)
@@ -29,5 +26,58 @@ public class Bomb : MonoBehaviour
 
         Enemy enemy = collider.GetComponent<Enemy>();
         enemy.TakeDamage(_damage);
+    }
+
+    private IEnumerator ExpandBomb()
+    {
+        float timer = 0f;
+
+        while (timer < _expandTime)
+        {
+            float t = timer / _expandTime;
+
+            float newScale = Mathf.SmoothStep(0, _minMaxScale.x, t);
+            transform.localScale = new Vector3(newScale, newScale, newScale);
+
+            timer += Time.deltaTime;
+            yield return null;
+        }
+
+        StartCoroutine(AnimateBombIdle());
+    }
+
+    private IEnumerator AnimateBombIdle()
+    {
+        float timer = 0f;
+
+        while (timer < _duration)
+        {
+            float newScale = Mathf.PingPong(timer, _minMaxScale.y - _minMaxScale.x) + _minMaxScale.x;
+            transform.localScale = new Vector3(newScale, newScale, newScale);
+
+            timer += Time.deltaTime;
+            yield return null;
+        }
+
+        StartCoroutine(ShrinkBomb());
+    }
+
+    private IEnumerator ShrinkBomb()
+    {
+        float timer = 0f;
+        float startScale = transform.localScale.x;
+
+        while (timer < _shrinkTime)
+        {
+            float t = timer / _shrinkTime;
+
+            float newScale = Mathf.Lerp(startScale, 0, t);
+            transform.localScale = new Vector3(newScale, newScale, newScale);
+
+            timer += Time.deltaTime;
+            yield return null;
+        }
+
+        Destroy(gameObject);
     }
 }

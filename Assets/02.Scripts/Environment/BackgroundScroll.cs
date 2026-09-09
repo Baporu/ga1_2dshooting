3313b217f71cd5f11a4a43431539ec69e9691714
mongilteacher <mongilteacher@gmail.com>
2026-09-09 14:06:21 +0900

    feat: 패럴랙스 스크롤링 (시차 효과)

--- Assets/02.Scripts/Environment/BackgroundScroll.cs
+++ Assets/02.Scripts/Environment/BackgroundScroll.cs
@@ -15,6 +15,8 @@ public class BackgroundScroll : MonoBehaviour
     private void Update()
     {
         _offsetY += _scrollSpeed * Time.deltaTime;
+
+        // ToDo: MaterialPropertyBlock을 활용한 최적화
         _material.mainTextureOffset = new Vector2(0, _offsetY);
     }
 }

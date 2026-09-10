f93e0312aab43ffeaa9b1c31d260cf47eab1f05f
LAPTOP-F2GOMJH5\qkswk <vitaqin@naver.com>
2026-09-10 14:48:35 +0900

    feat: 최고 점수 저장/로드 추가
    - PlayerPrefs 이용
    - 코드 위치 자잘하게 수정

--- Assets/02.Scripts/Manager/ScoreManager.cs
+++ Assets/02.Scripts/Manager/ScoreManager.cs
@@ -9,13 +9,15 @@ public class ScoreManager : MonoBehaviour
     private static ScoreManager _instance;
     public static ScoreManager Instance => _instance;
 
+    // UI 책임 추가 (TMP 참조)
+    [SerializeField] private TextMeshProUGUI _bestScoreTextUI;
+    [SerializeField] private TextMeshProUGUI _currentScoreTextUI;
+
     // 관리: 특정 데이터에 대한 무결성과 생성, 읽기, 수정, 삭제 등과 관련된 게임 로직
     private int _bestScore;
     private int _currentScore = 0;
 
-    // UI 책임 추가 (TMP 참조)
-    [SerializeField] private TextMeshProUGUI _bestScoreTextUI;
-    [SerializeField] private TextMeshProUGUI _currentScoreTextUI;
+    private const string SaveKey = "BestScore";
 
 
     private void Awake()
@@ -30,15 +32,30 @@ public class ScoreManager : MonoBehaviour
         DontDestroyOnLoad(gameObject);
     }
 
+    private void Start()
+    {
+        // 입력: Input
+        // 저장, 불러오기: PlayerPrefs
+        _bestScore = PlayerPrefs.GetInt(SaveKey, 0);
+
+        Refresh();
+    }
+
     public void AddScore(int score)
     {
         if (score < 0)
             return;
 
         _currentScore += score;
+
         if (_currentScore > _bestScore)
         {
             _bestScore = _currentScore;
+
+            // 저장: PlayerPrefs.Set~ 시리즈를 이용해서 int/float/string을 저장 가능하다.
+            // 컴퓨터는 레지스트리, 모바일은 로컬 저장소에 저장된다.
+            PlayerPrefs.SetInt(SaveKey, _bestScore);
+            PlayerPrefs.Save();
         }
 
         Refresh();

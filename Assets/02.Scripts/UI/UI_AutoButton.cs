7743c67402b333dee4a72e8ba7ac038dd60521de
mongilteacher <mongilteacher@gmail.com>
2026-09-14 11:04:53 +0900

    feat: 애니메이션 커브를 이용한 버튼 애니메이션
    - 클릭하면 커지게

--- Assets/02.Scripts/UI/UI_AutoButton.cs
+++ Assets/02.Scripts/UI/UI_AutoButton.cs
@@ -12,13 +12,26 @@ public class UI_AutoButton : MonoBehaviour
     [SerializeField] private Sprite _offSprite;
 
     private Image _myImage;
+    private AudioSource _audioSource;
+
     private bool _autoMode = false;
     private Player _player;
+
+    [Header("클릭시 애니메이션")]
+    [SerializeField] private AnimationCurve _bumpCurve;
+
+    private float _scale = 1.0f;
+    private bool _isBumping = false;
+    private float _elapsedTime = 0; // 경과 시간
+    private const float BumpDuration = 0.3f;
+    private const float BumpScale = 1.1f;
+
 
     private void Start()
     {
         _myImage = GetComponent<Image>();
-        _player = GameObject.FindAnyObjectByType<Player>();
+        _audioSource = GetComponent<AudioSource>();
+        _player = FindAnyObjectByType<Player>();
 
         AutoToggle();
     }
@@ -39,5 +52,36 @@ public class UI_AutoButton : MonoBehaviour
 
     // todo: 버튼 클릭할 때 애니메이션 주기 + 사운드 주기
     // 애니메이션: 코드로 구현 약간 커졌따가 원래대로..
+
+    public void PlayAnimation()
+    {
+        _isBumping = true;
+        _elapsedTime = 0;
+    }
+
+    private void Update()
+    {
+        if (!_isBumping) return;
+
+        // 1. 경과 시간 누적
+        _elapsedTime += Time.deltaTime;
+        if (_elapsedTime > BumpDuration) // 시간이 다 지났다면..
+        {
+            transform.localScale = Vector3.one; // 스케일 초기화
+            _isBumping = false;
+            return;
+        }
+
+        // 2. 누적 시간과 애니메이션 커브에 따른 스케일 변경
+        float time = _elapsedTime / BumpDuration; // 얼마나 지났는지 퍼센트 (0 ~ 1)
+        float curveValue = _bumpCurve.Evaluate(time); // 퍼센트에 따라 커브 애니메이션 값 추출
+        transform.localScale = Vector3.Lerp(transform.localScale, Vector3.one * BumpScale, curveValue);
+    }
+
+
     // 사운드: 일레븐랩스에서 버튼 클릭 공용 사운드 만들어서 적용
+    public void PlaySound()
+    {
+        _audioSource.Play();
+    }
 }

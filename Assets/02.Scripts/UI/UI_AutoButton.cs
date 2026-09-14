912e78693ec9f3654ca1b1320731f18f462fb480
LAPTOP-F2GOMJH5\qkswk <vitaqin@naver.com>
2026-09-14 11:16:16 +0900

    feat: 버튼 클릭 시 애니메이션 추가
    - Animation Curve를 이용한 Ease in & out 적용

--- Assets/02.Scripts/UI/UI_AutoButton.cs
+++ Assets/02.Scripts/UI/UI_AutoButton.cs
@@ -14,6 +14,15 @@ public class UI_AutoButton : MonoBehaviour
     private bool _autoMode = false;
     private Player _player;
 
+    [Header("클릭 시 애니메이션")]
+    [SerializeField] private AnimationCurve _bumpCurve;
+
+    private bool _isBumping = false;
+    private float _elapsedTime = 0f;
+
+    private const float BumpScale = 1.1f;
+    private const float BumpDuration = 0.15f;
+
 
     private void Start()
     {
@@ -21,6 +30,27 @@ public class UI_AutoButton : MonoBehaviour
         _player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
     }
 
+    private void Update()
+    {
+        if (!_isBumping)
+            return;
+
+        // 1. 경과 시간 누적
+        _elapsedTime += Time.deltaTime;
+
+        if (_elapsedTime > BumpDuration)
+        {
+            transform.localScale = Vector3.one;
+            _isBumping = false;
+            return;
+        }
+
+        // 2. 누적 시간과 애니메이션 커브에 따른 스케일 변경
+        float time = _elapsedTime / BumpDuration; // 얼마나 지났는지 퍼센트 (0 ~ 1)
+        float curveValue = _bumpCurve.Evaluate(time); // 퍼센트에 따라 커브 애니메이션 값 추출
+
+        transform.localScale = Vector3.Lerp(Vector3.one, Vector3.one * BumpScale, curveValue);
+    }
 
     public void AutoToggle()
     {
@@ -37,5 +67,11 @@ public class UI_AutoButton : MonoBehaviour
     public void PlaySound()
     {
         AudioManager.Instance.PlaySFX(AudioType.UI_BUTTON_CLICKED);
+    }
+
+    public void PlayAnimation()
+    {
+        _isBumping = true;
+        _elapsedTime = 0f;
     }
 }

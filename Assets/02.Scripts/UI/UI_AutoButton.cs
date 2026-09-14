aabe720f118b51bf2fabd3e4ffc297c75af93577
LAPTOP-F2GOMJH5\qkswk <vitaqin@naver.com>
2026-09-14 11:20:22 +0900

    refactor: 버튼 클릭 이벤트 분리
    - 클릭 시 사운드, 애니메이션 재생 이벤트를 별도 클래스로 분리

--- Assets/02.Scripts/UI/UI_AutoButton.cs
+++ Assets/02.Scripts/UI/UI_AutoButton.cs
@@ -14,42 +14,11 @@ public class UI_AutoButton : MonoBehaviour
     private bool _autoMode = false;
     private Player _player;
 
-    [Header("클릭 시 애니메이션")]
-    [SerializeField] private AnimationCurve _bumpCurve;
-
-    private bool _isBumping = false;
-    private float _elapsedTime = 0f;
-
-    private const float BumpScale = 1.1f;
-    private const float BumpDuration = 0.15f;
-
 
     private void Start()
     {
         _myImage = GetComponent<Image>();
         _player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
-    }
-
-    private void Update()
-    {
-        if (!_isBumping)
-            return;
-
-        // 1. 경과 시간 누적
-        _elapsedTime += Time.deltaTime;
-
-        if (_elapsedTime > BumpDuration)
-        {
-            transform.localScale = Vector3.one;
-            _isBumping = false;
-            return;
-        }
-
-        // 2. 누적 시간과 애니메이션 커브에 따른 스케일 변경
-        float time = _elapsedTime / BumpDuration; // 얼마나 지났는지 퍼센트 (0 ~ 1)
-        float curveValue = _bumpCurve.Evaluate(time); // 퍼센트에 따라 커브 애니메이션 값 추출
-
-        transform.localScale = Vector3.Lerp(Vector3.one, Vector3.one * BumpScale, curveValue);
     }
 
     public void AutoToggle()
@@ -62,16 +31,5 @@ public class UI_AutoButton : MonoBehaviour
 
         // 오토 모드에 따라 보여지는 이미지 스프라이트 교체
         _myImage.sprite = _autoMode ? _onSprite : _offSprite;
-    }
-
-    public void PlaySound()
-    {
-        AudioManager.Instance.PlaySFX(AudioType.UI_BUTTON_CLICKED);
-    }
-
-    public void PlayAnimation()
-    {
-        _isBumping = true;
-        _elapsedTime = 0f;
     }
 }
---
+++ Assets/02.Scripts/UI/UI_ButtonClick.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+public class UI_ButtonClick : MonoBehaviour
+{
+    [Header("클릭 시 애니메이션")]
+    [SerializeField] private AnimationCurve _bumpCurve;
+
+    private bool _isBumping = false;
+    private float _elapsedTime = 0f;
+
+    private const float BumpScale = 1.1f;
+    private const float BumpDuration = 0.15f;
+
+
+    private void Update()
+    {
+        if (!_isBumping)
+            return;
+
+        // 1. 경과 시간 누적
+        _elapsedTime += Time.deltaTime;
+
+        if (_elapsedTime > BumpDuration)
+        {
+            transform.localScale = Vector3.one;
+            _isBumping = false;
+            return;
+        }
+
+        // 2. 누적 시간과 애니메이션 커브에 따른 스케일 변경
+        float time = _elapsedTime / BumpDuration; // 얼마나 지났는지 퍼센트 (0 ~ 1)
+        float curveValue = _bumpCurve.Evaluate(time); // 퍼센트에 따라 커브 애니메이션 값 추출
+
+        transform.localScale = Vector3.Lerp(Vector3.one, Vector3.one * BumpScale, curveValue);
+    }
+
+    public void PlaySound()
+    {
+        AudioManager.Instance.PlaySFX(AudioType.UI_BUTTON_CLICKED);
+    }
+
+    public void PlayAnimation()
+    {
+        _isBumping = true;
+        _elapsedTime = 0f;
+    }
+}
